cefe61992e428d7b5208908a37b03099515407e9
flamencist <flamencist@mail.ru>
2017-05-16 19:26:35 +0300

    updated selector-generator lib

--- src/js/content.js
+++ src/js/content.js
@@ -12,14 +12,40 @@ function copyToClipboard(text) {
     document.body.removeChild(input);
 }
 
-document.addEventListener("mousedown", function (event) {
+function addClass(element, cls){
+    let classes = (element.className || "").split(" ");
+    if(!classes.includes(cls)){
+        element.className = classes.concat([cls]).join(" ");
+    }
+}
+
+function removeClass(element, cls){
+    let classes = (element.className || "").split(" ");
+    if(classes.includes(cls)){
+        element.className = classes.filter(_=>_ !== cls).join(" ");
+    }
+}
+
+function highlight(element){
+    if(!element){
+        return;
+    }
+    const higlightClass = "__copy-css-selector-highlighted";
+    addClass(element,higlightClass);
+    setTimeout(() => {
+        removeClass(element,higlightClass);
+    },2000);
+}
+
+document.addEventListener("mousedown", (event) => {
     clickedElement = event.target;
 }, true);
 
-chrome.runtime.onMessage.addListener(function (request) {
+chrome.runtime.onMessage.addListener((request) => {
     if(request && request.target === "copy"){
         let selectorGenerator = new SelectorGenerator({querySelectorAll:window.document.querySelectorAll.bind(window.document)});
         let selector = selectorGenerator.getSelector(clickedElement);
+        highlight(clickedElement);
         copyToClipboard(selector);
     }
 });
