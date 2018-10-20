b1b41a0bc834405e3e2186bb3b9676b6bdccd6e2
Alexander Chermyanin <flamencist@mail.ru>
2018-10-20 09:50:11 +0000

    fix loading on existed pages on extension installation

--- src/js/background.js
+++ src/js/background.js
@@ -8,3 +8,25 @@ chrome.contextMenus.create({
     contexts: ["all"],
     "onclick": onCopy
 });
+
+chrome.runtime.onInstalled.addListener((details)=>{
+    if(["install", "update"].some((reason)=>details.reason === reason)){
+        injectScriptsInAllTabs();
+    }
+});
+
+function injectScriptsInAllTabs(){
+    console.log("reinject content scripts into all tabs");
+    var manifest = chrome.runtime.getManifest();
+    var scripts = manifest.content_scripts.reduce((sc, cur)=>sc.concat(cur.js || []), []);
+    var styles = manifest.content_scripts.reduce((sc, cur)=>sc.concat(cur.css || []), []);
+    chrome.tabs.query({url:"*://*/*"}, (tabs)=>{
+        var filtered = tabs.filter(_=>_.url.indexOf("https://chrome.google.com/webstore/detail") !== 0);
+        filtered.forEach(tab=>{
+            scripts.map((sc)=>chrome.tabs.executeScript(tab.id, {file: sc, allFrames: true}));
+        });
+        filtered.forEach(tab=>{
+            styles.map((sc)=>chrome.tabs.insertCSS(tab.id, {file: sc, allFrames: true}));
+        });
+    });
+}
